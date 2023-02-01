3d4b4dbf0eed6fb34047f723dbbf007a0dbd033a
Enrico Beyer <enrico.beyer@synaigy.com>
2023-02-01 15:51:51 +0100

    Change class selection to inline to prevent missing classes

--- apps/web/src/lib/components/Alert.svelte
+++ apps/web/src/lib/components/Alert.svelte
@@ -1,6 +1,5 @@
-<script>
-	// @ts-nocheck
-
+<script lang="ts">
+	import { classNames } from '$lib/utils';
 	import {
 		CheckCircle,
 		ExclamationCircle,
@@ -8,18 +7,12 @@
 		XCircle,
 		XMark
 	} from '@steeze-ui/heroicons';
+	import type { IconSource } from '@steeze-ui/heroicons/types';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
 	export let type = 'success';
 
-	const types = {
-		success: 'green',
-		warning: 'yellow',
-		error: 'red',
-		info: 'blue'
-	};
-
-	const icons = {
+	const icons: Record<string, IconSource> = {
 		success: CheckCircle,
 		warning: ExclamationCircle,
 		error: XCircle,
@@ -27,13 +20,59 @@
 	};
 </script>
 
-<div class="rounded-md bg-{types[type]}-100 p-4 mt-4">
+<div
+	class={classNames(
+		type === 'success' && 'bg-green-50',
+		type === 'warning' && 'bg-yellow-50',
+		type === 'error' && 'bg-red-50',
+		type === 'info' && 'bg-blue-50',
+		'rounded-md p-4 mt-4'
+	)}
+>
 	<div class="flex">
 		<div class="flex-shrink-0">
-			<Icon src={icons[type]} theme="mini" class="h-5 w-5 text-{types[type]}-400" />
+			<Icon
+				src={icons[type]}
+				theme="mini"
+				class={classNames(
+					type === 'success' && 'text-green-400',
+					type === 'warning' && 'text-yellow-400',
+					type === 'error' && 'text-red-400',
+					type === 'info' && 'text-blue-400',
+					'w-5 h-5'
+				)}
+			/>
 		</div>
 		<div class="ml-3">
-			<p class="text-sm font-medium text-{types[type]}-800"><slot /></p>
+			{#if $$slots.default}
+				<h3
+					class={classNames(
+						type === 'success' && 'text-green-800',
+						type === 'warning' && 'text-yellow-800',
+						type === 'error' && 'text-red-800',
+						type === 'info' && 'text-blue-800',
+						'text-sm font-medium'
+					)}
+				>
+					<slot />
+				</h3>
+			{/if}
+			{#if $$slots.description}
+				<div
+					class={classNames(
+						$$slots.default && 'mt-2',
+						type === 'success' && 'text-green-700',
+						type === 'warning' && 'text-yellow-700',
+						type === 'error' && 'text-red-700',
+						type === 'info' && 'text-blue-700',
+						'text-sm'
+					)}
+				>
+					<p>
+						<slot name="description" />
+					</p>
+				</div>
+			{/if}
 		</div>
 		<!-- <div class="pl-3 ml-auto">
 			<div class="-mx-1.5 -my-1.5">
