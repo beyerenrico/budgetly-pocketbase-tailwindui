7103bf0ef66f3f999a17d08e2740b679db203fb1
Enrico Beyer <enrico.beyer@synaigy.com>
2023-02-02 16:39:11 +0100

    Add additional media query

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -2,7 +2,14 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		}
 	},
 
 	plugins: [require('@tailwindcss/forms')]
