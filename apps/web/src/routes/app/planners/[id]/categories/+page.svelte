df1a3c67b374d3934179588057feb620beb7927b
Enrico Beyer <enrico.beyer@synaigy.com>
2023-02-02 16:41:47 +0100

    Add planner subpages

---
+++ apps/web/src/routes/app/planners/[id]/categories/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { AppBreadcrumbs } from '$lib/components';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const breadcrumbElements = [
+		{
+			title: 'Planners',
+			href: '/app/planners'
+		},
+		{
+			title: data?.planner?.title,
+			href: `/app/planners/${data?.planner?.id}`
+		},
+		{
+			title: 'Categories',
+			href: `/app/planners/${data?.planner?.id}/categories`
+		}
+	];
+</script>
+
+<AppBreadcrumbs {breadcrumbElements} />
