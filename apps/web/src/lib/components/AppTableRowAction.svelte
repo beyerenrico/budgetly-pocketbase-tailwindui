5171845c4c4ce8563fa1be83da04a431890a5641
Enrico Beyer <enrico.beyer@synaigy.com>
2023-02-04 21:28:37 +0100

    Replace a tag with button and forward on:click

--- apps/web/src/lib/components/AppTableRowAction.svelte
+++ apps/web/src/lib/components/AppTableRowAction.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	export let link: string;
 	export let identifier: string;
 </script>
 
 <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-	<a href={link} class="text-indigo-600 hover:text-indigo-900">
+	<button on:click type="button" class="text-indigo-600 hover:text-indigo-900">
 		<slot /><span class="sr-only">, {identifier}</span>
-	</a>
+	</button>
 </td>
